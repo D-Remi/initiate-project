8de4e0e4301fda379bd5833e07e9e054b68241e3
rdelfosse <remi.delfosse.ext@d2lgroup.com>
2023-01-21 19:22:30 +0100

    fix react component

--- assets/component/HomeComponent.js
+++ assets/component/HomeComponent.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HomeComponent = () => {
     return (
         <div>
-            <h1>Wellcome to start project</h1>
+            <h1>Wellcome to start projects</h1>
         </div>
     );
 };
