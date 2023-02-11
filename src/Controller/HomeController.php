2fe57310b8f37a1eba4c252fd379a8b7de2fae2d
rdelfosse <remi.delfosse.ext@d2lgroup.com>
2023-02-11 10:40:32 +0100

    add react routing on annotation home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-	#[Route('/', name: 'app_home')]
+	/**
+	* @Route("/{reactRouting}", name="app_home", requirements={"reactRouting"="^(?!api).+"}, defaults={"reactRouting"=null})
+	*/
 	public function index(): Response
 	{
 		return $this->render('base.html.twig');
